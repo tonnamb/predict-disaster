5c3863831ef2020ba28537e26226b2890575bc30
tonnamb <tonnam.balankura@gmail.com>
2016-04-09 15:25:27 -0400

    api_access.py with export

--- api_access.py
+++ api_access.py
@@ -1,3 +1,7 @@
+import os
+
+os.system('export ACCUWEATHER_APIKEY=c3e7df4f2d6a40698cc75fac1b6a2c83')
+
 from pyccuweather.connector import Connection
 
 conn = Connection(API_KEY="c3e7df4f2d6a40698cc75fac1b6a2c83")
